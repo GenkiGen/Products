43cee96dbae97883de6dc8b5a8b742fe2e60032b
JohnGenki <ryanz201278@yahoo.com.vn>
2019-04-12 16:16:25 +0700

    Added imageUrl to schema

--- models/product.model.js
+++ models/product.model.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema
 const ProductSchema = new Schema({
   name: { type: String, required: true, maxlength: 1000 },
   price: { type: String, required: true },
+  imageUrl: { type: String, required: true },
   productType: { type: String, required: true },
   description: { type: String, required: true },
   created_at: { type: Date, required: true, default: Date.now }
